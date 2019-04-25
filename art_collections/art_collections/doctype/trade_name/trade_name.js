fd8a23208bc2fce59f6cedcbd5e173d8adb5e2bf
ashish-greycube <mr.ashish.shah@gmail.com>
2019-04-25 03:44:10 +0000

    jignesh doctypes

---
+++ art_collections/art_collections/doctype/trade_name/trade_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Trade Name', {
+	refresh: function(frm) {
+
+	}
+});

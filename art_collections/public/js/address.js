308ff116d40a0e93cefd92f3bac26b90836acbd9
ashish-greycube <mr.ashish.shah@gmail.com>
2019-04-30 23:20:32 +0530

    duplicate state and country

---
+++ art_collections/public/js/address.js
@@ -0,0 +1,13 @@
+frappe.ui.form.on('Address', {
+
+   validate: function (frm) {
+    if (frm.doc.art_county) 
+    {
+        frm.doc.country=frm.doc.art_county
+    }
+    if (frm.doc.art_state) 
+    {
+        frm.doc.state=frm.doc.art_state
+    }
+   }
+});

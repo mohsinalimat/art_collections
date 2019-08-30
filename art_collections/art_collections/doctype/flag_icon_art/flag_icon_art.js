d188d10179d442eeaabf935a22f843ddf4bb8bf4
ashish-greycube <mr.ashish.shah@gmail.com>
2019-08-30 04:20:30 +0000

    Item + events

---
+++ art_collections/art_collections/doctype/flag_icon_art/flag_icon_art.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Flag Icon Art', {
+	// refresh: function(frm) {
+
+	// }
+});

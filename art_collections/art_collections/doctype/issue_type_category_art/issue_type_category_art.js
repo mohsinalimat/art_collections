1b76cbcc47ed7a450e7fd3344bb8ab952b826e76
ashish-greycube <mr.ashish.shah@gmail.com>
2019-08-30 03:53:54 +0000

    Item + Issue Type Category

---
+++ art_collections/art_collections/doctype/issue_type_category_art/issue_type_category_art.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Issue Type Category Art', {
+	// refresh: function(frm) {
+
+	// }
+});

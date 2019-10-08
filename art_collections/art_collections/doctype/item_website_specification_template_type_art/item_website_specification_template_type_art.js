16d20f1eb56bc5956cdede4977213ddf10c90aba
ashish-greycube <mr.ashish.shah@gmail.com>
2019-10-08 08:50:50 +0000

    removed Item Website Specification Template

--- art_collections/art_collections/doctype/item_website_specification_template_type_art/item_website_specification_template_type_art.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, GreyCube Technologies and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Item Website Specification Template Type Art', {
-	// refresh: function(frm) {
-
-	// }
-});

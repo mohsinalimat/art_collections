e14606577dc8471dea1ac1e921f26d5fca1da4d5
ashish-greycube <mr.ashish.shah@gmail.com>
2019-10-23 04:09:23 +0000

    cleansing + issue resolved

--- art_collections/art_collections/doctype/univers_art/univers_art.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, GreyCube Technologies and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Univers Art', {
-	// refresh: function(frm) {
-
-	// }
-});

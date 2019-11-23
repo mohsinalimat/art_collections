28ff8cbaa9d932569d9606e51baba291c9bb1b37
ashish-greycube <mr.ashish.shah@gmail.com>
2019-11-23 15:45:03 +0530

    stock error and consider stock_availablity

--- art_collections/api.py
+++ art_collections/api.py
@@ -162,7 +162,6 @@ def update_cart(item_code, qty, additional_notes=None, with_items=False):
 
 #Helper for finding item qty
 def get_qty_in_stock(item_code, item_warehouse_field, warehouse=None):
-	print(item_code, item_warehouse_field, warehouse)
 	from erpnext.utilities.product import get_price, adjust_qty_for_expired_items
 	in_stock, stock_qty = 0, ''
 	template_item_code, is_stock_item = frappe.db.get_value("Item", item_code, ["variant_of", "is_stock_item"])
@@ -174,7 +173,6 @@ def get_qty_in_stock(item_code, item_warehouse_field, warehouse=None):
 		warehouse = frappe.db.get_value("Item", template_item_code, item_warehouse_field)
 	if warehouse:
 		p_warehouse = frappe.get_doc("Warehouse", warehouse)
-		print(item_code, p_warehouse.lft, p_warehouse.rgt)
 		stock_qty = frappe.db.sql("""
 			select SUM(GREATEST(S.actual_qty - S.reserved_qty - S.reserved_qty_for_production - S.reserved_qty_for_sub_contract, 0) / IFNULL(C.conversion_factor, 1))
 			from tabBin S
@@ -185,8 +183,9 @@ def get_qty_in_stock(item_code, item_warehouse_field, warehouse=None):
 			""",(item_code, p_warehouse.lft, p_warehouse.rgt))
 		if stock_qty:
 			stock_qty = adjust_qty_for_expired_items(item_code, stock_qty, warehouse)
-			in_stock = stock_qty[0][0] > 0 and 1 or 0
-
+			if stock_qty:
+				if stock_qty[0][0]:
+					in_stock = stock_qty[0][0] > 0 and 1 or 0
 	return frappe._dict({"in_stock": in_stock, "stock_qty": stock_qty, "is_stock_item": is_stock_item})
 		
 

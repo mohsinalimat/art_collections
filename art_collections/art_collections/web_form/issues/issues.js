a59f395273e42dd1fe22b54db8ec939db03d095f
ashish-greycube <mr.ashish.shah@gmail.com>
2019-12-10 15:21:45 +0530

    issue type based on category for desk and web

--- art_collections/art_collections/web_form/issues/issues.js
+++ art_collections/art_collections/web_form/issues/issues.js
@@ -2,13 +2,42 @@ frappe.ready(function () {
 	// bind events here
 	frappe.init_client_script = () => {
 		try {
-			frappe.web_form.on('issue_type_art', (field, value) => {
+			frappe.web_form.on('issue_type', (field, value) => {
 				frappe.web_form.set_value('subject', value);
 			});
 			frappe.web_form.validate = () => {
-				let data = frappe.web_form.get_value('issue_type_art');
+				let data = frappe.web_form.get_value('issue_type');
 				frappe.web_form.set_value('subject', data);
 			};
+
+			frappe.web_form.on('issue_type_category_art', (field, value) => {
+				filterIssueType(value);
+
+				function filterIssueType(value) {
+					let url='api/resource/Issue Type?filters=[["Issue Type","Category_Art","=","'+value+'"]]'
+					$.ajax({
+					url: url,
+					success: function (result) {
+						frappe.web_form.doc.issue_type=''
+						var options = []
+						for (var i = 0; i < result.data.length; i++) {
+							options.push({
+								"label": result.data[i].name,
+								"value": result.data[i].name
+							})
+
+						}
+						var field = frappe.web_form.get_field('issue_type');
+						field._data = options;
+						field.refresh();
+
+					},
+					error:function (opts) {
+						console.log(opts,'-------------')
+					}
+					});
+					};				
+				});
 
 		} catch (e) {
 			console.error('Error in web form client script');
--- art_collections/public/js/issue.js
+++ art_collections/public/js/issue.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Issue', {
 	validate: function (frm) {
-		frm.set_value('subject', frm.doc.issue_type_art)
+		frm.set_value('subject', frm.doc.issue_type)
 	},
     issue_type_category_art: function (frm) {
 
@@ -12,17 +12,7 @@ frappe.ui.form.on('Issue', {
 			};
 		});
 	},
-		issue_type_art: function (frm) {
-			frm.set_value('subject', frm.doc.issue_type_art)	
+		issue_type: function (frm) {
+			frm.set_value('subject', frm.doc.issue_type)	
 		}
-		// if (frm.doc.issue_type_category_art) {
-		// 	frappe.db.get_value('Issue Type', {'category_art': frm.doc.issue_type_category_art}, ['name'])
-		// 	.then(({ message }) => {
-		// 		var issue_type = message.name
-		// 		if ( issue_type ) {
-		// 			frm.set_value('issue_type', issue_type)	
-		// 		}
-		// 	});	
-		// }
-   
  });

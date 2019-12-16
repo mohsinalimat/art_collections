090e1219fce0a976169c35e88ce8af5332b35cfb
ashish-greycube <mr.ashish.shah@gmail.com>
2019-12-16 20:29:33 +0530

    naming series in pos profile

--- art_collections/public/js/pos_profile.js
+++ art_collections/public/js/pos_profile.js
@@ -4,7 +4,7 @@ frappe.ui.form.on("POS Profile", "onload", function(frm) {
 		method: "art_collections.api.pos_so_get_series",
 		callback: function(r) {
 			if(!r.exc) {
-				set_field_options("naming_series", r.message);
+				set_field_options("pos_so_naming_series_art", r.message);
 			}
 		}
 	});

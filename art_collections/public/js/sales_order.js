12d31228f7ed0365259380523ce7ef5ce4d4a73f
ashish-greycube <mr.ashish.shah@gmail.com>
2021-05-20 15:43:02 +0530

    toggle_reqd(order_expiry_date_ar, frm.doc.needs_confirmation_art === 1)

--- art_collections/public/js/sales_order.js
+++ art_collections/public/js/sales_order.js
@@ -1,4 +1,10 @@
 frappe.ui.form.on('Sales Order', {
+	refresh: function (frm) {
+		frm.toggle_reqd('order_expiry_date_ar', frm.doc.needs_confirmation_art === 1);
+	},
+	needs_confirmation_art: function (frm) {
+		frm.toggle_reqd('order_expiry_date_ar', frm.doc.needs_confirmation_art === 1);
+	},
     validate: function (frm) {
 		$.each(frm.doc.items || [], function(i, d) {
 			frappe.db.get_value('Item', d.item_code, 'max_qty_allowed_in_shopping_cart_art').then

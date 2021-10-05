cca2ce56596d42fd569073a88e5b92579f862a33
ashish-greycube <mr.ashish.shah@gmail.com>
2021-10-05 12:43:49 +0200

    fix: json changes for #20

--- art_collections/art_collections/doctype/port_art/port_art.js
+++ art_collections/art_collections/doctype/port_art/port_art.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, GreyCube Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Port Of Loading', {
+frappe.ui.form.on('Port Art', {
 	// refresh: function(frm) {
 
 	// }
--- art_collections/art_collections/doctype/port_art/test_port_art.py
+++ art_collections/art_collections/doctype/port_art/test_port_art.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2021, GreyCube Technologies and Contributors
 # See license.txt
-from __future__ import unicode_literals
 
 # import frappe
 import unittest
 
-class TestPortOfLoading(unittest.TestCase):
+class TestPortArt(unittest.TestCase):
 	pass

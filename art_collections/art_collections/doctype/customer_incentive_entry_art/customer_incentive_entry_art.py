24012655f3ee75fb790cf7babb0a09da73e8702d
ashish-greycube <mr.ashish.shah@gmail.com>
2021-10-26 10:38:19 +0530

    fix: donot fetch when discount or achieved amuont is zero

--- art_collections/art_collections/doctype/customer_incentive_entry_art/customer_incentive_entry_art.py
+++ art_collections/art_collections/doctype/customer_incentive_entry_art/customer_incentive_entry_art.py
@@ -55,7 +55,7 @@ class CustomerIncentiveEntryArt(Document):
 					total_achieved_amount+=sales_invoice.base_net_total
 					
 
-				if total_achieved_amount>=customer.from_value and (total_achieved_amount<=customer.to_value or customer.to_value==0):
+				if total_achieved_amount>=customer.from_value and (total_achieved_amount<=customer.to_value or customer.to_value==0) and total_achieved_amount>0 and achieved_discount_amount>0 :
 					achieved_discount_amount=(total_achieved_amount*customer.discount_percent)/100
 					self.append('customer_goal_achievement_detail',{
 					'customer':customer_name,

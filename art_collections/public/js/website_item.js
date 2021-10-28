37b8a2a08324423f58d43319a1a2bdd774a03ee6
ashish-greycube <mr.ashish.shah@gmail.com>
2021-10-28 15:45:11 +0530

    fix: remove extra description

--- art_collections/public/js/website_item.js
+++ art_collections/public/js/website_item.js
@@ -2,35 +2,37 @@ frappe.ui.form.on('Website Item', {
 	copy_item_specification_art: function (frm) {
 		if (frm.doc.item_code) {
 			let row1_desc, row2_desc, row3_desc = undefined
-			frappe.db.get_value('Item', frm.doc.item_code, ['qty_in_selling_pack_art', 'main_design_color_art', 'length_art', 'width_art', 'thickness_art', 'item_group', ])
+			frappe.db.get_value('Item', frm.doc.item_code, ['qty_in_selling_pack_art', 'main_design_color_art', 'length_art', 'width_art', 'thickness_art' ])
 				.then(r => {
+					debugger
 					let values = r.message;
 					if (values.qty_in_selling_pack_art) {
-						row1_desc = values.qty_in_selling_pack_art + __(' qty per pack for this item in ') + values.item_group + __(' group')
+						row1_desc = values.qty_in_selling_pack_art
 					}
 					if (values.main_design_color_art) {
-						row2_desc = values.main_design_color_art + __(' is the main color')
+						row2_desc = values.main_design_color_art
 					}
 					if (values.length_art) {
-						row3_desc = (values.length_art || 0) + __('L x ') + (values.width_art || 0) + __('W x ') + (values.thickness_art || 0) + __('T')
+						row3_desc = (cstr(values.length_art) || 0) + __('L x ') + (cstr(values.width_art) || 0) + __('W x ') + (cstr(values.thickness_art) || 0) + __('T')
 					}
 					if (row1_desc) {
 						let row1 = frm.add_child('website_specifications')
 						row1.label = __('Qty per pack')
-						row1.description = row1_desc
+						row1.description = '<div class="ql-editor read-mode"><p>'+row1_desc+'</p></div>'
 					}
 					if (row2_desc) {
 						let row2 = frm.add_child('website_specifications')
 						row2.label = __('Main Color')
-						row2.description = row2_desc
+						row2.description ='<div class="ql-editor read-mode"><p>'+row2_desc+'</p></div>' 
 					}
 					if (row3_desc) {
 						let row3 = frm.add_child('website_specifications')
 						row3.label = __('Product Dimension')
-						row3.description = row3_desc
+						row3.description ='<div class="ql-editor read-mode"><p>'+row3_desc+'</p></div>' 
 					}
 					frm.refresh_field('website_specifications')
 				})
 		}
 	}
 });
+

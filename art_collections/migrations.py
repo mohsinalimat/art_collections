936da04bbdeb81f1480d4597146a40f015a95384
ashish-greycube <mr.ashish.shah@gmail.com>
2021-11-22 13:05:45 +0530

    feat : #101 import dashboard link for core doctype

--- art_collections/migrations.py
+++ art_collections/migrations.py
@@ -6,6 +6,7 @@ from os.path import join
 
 
 def after_migrations():
+	update_dashboard_link_for_core_doctype(doctype='Supplier',link_doctype='Supplier Item Directive',link_fieldname='supplier')
 	if(not frappe.db.exists('Workflow','BDC')):
 		fname="workflow.json"
 		import_folder_path="{bench_path}/{app_folder_path}".format(bench_path=get_bench_path(),app_folder_path='/apps/art_collections/art_collections/import_records')
@@ -25,3 +26,20 @@ def after_migrations():
 def make_records(path, fname):
 	if os.path.isdir(path):
 		import_file_by_path("{path}/{fname}".format(path=path, fname=fname))
+
+
+def update_dashboard_link_for_core_doctype(doctype,link_doctype,link_fieldname,group=None):
+	try:
+		d = frappe.get_doc("Customize Form")
+		if doctype:
+			d.doc_type = doctype
+		d.run_method("fetch_to_customize")
+		for link in d.get('links'):
+			if link.link_doctype==link_doctype and link.link_fieldname==link_fieldname:
+				# found so just return
+				return
+		d.append('links', dict(link_doctype=link_doctype, link_fieldname=link_fieldname,table_fieldname=None,group=group))
+		d.run_method("save_customization")
+		frappe.clear_cache()
+	except Exception:
+		frappe.log_error(frappe.get_traceback())

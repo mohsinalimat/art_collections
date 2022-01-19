6c374d5744a411926edf64a2e40e887c41c630a9
ashish-greycube <mr.ashish.shah@gmail.com>
2022-01-19 18:35:38 +0530

    fix: remove false field

--- art_collections/excel_controller.py
+++ art_collections/excel_controller.py
@@ -233,7 +233,7 @@ def get_print_context_for_art_collectons_purchase_order(name):
         frappe.db.sql(
             """
         select i.item_name, i.customer_code, tib.barcode, i.customs_tariff_number,
-        tw.warehouse_name, poi.price_list_rate, poi.total_saleable_qty_cf, 
+        tw.warehouse_name, poi.price_list_rate,  
         poi.net_rate, poi.net_amount, poi.description, poi.total_weight, 
         poi.qty, poi.image, po.overall_directive_art
         from `tabPurchase Order Item` poi

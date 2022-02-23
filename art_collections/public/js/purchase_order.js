ba2d16e1502f17b73df153862e859b8ac89b6272
ashish-greycube <mr.ashish.shah@gmail.com>
2022-02-23 10:27:20 +0530

    ID 181: get_qty_of_inner_cartoon from item_code

--- art_collections/public/js/purchase_order.js
+++ art_collections/public/js/purchase_order.js
@@ -52,14 +52,23 @@ frappe.ui.form.on('Purchase Order', {
 			.then(r => {
 					row.min_order_qty_cf=r.message.min_order_qty
 			})	
+			
+			frappe.call('art_collections.item_controller.get_qty_of_inner_cartoon', {
+			    item_code: row.item_code
+			}).then(r => {
+			    if (r.message) {
+			    	row.nb_selling_packs_in_inner_art=r.message
+			    }
+			})	
+			
 			frappe.call('art_collections.item_controller.get_qty_of_outer_cartoon', {
 			    item_code: row.item_code
 			}).then(r => {
 			    if (r.message) {
 			    	row.nb_selling_packs_in_outer_art=r.message
 			    }
-			})					
-			
+			})
+
 		}
 		if (row.item_code && frm.doc.supplier) {
 			frappe.db.get_value('Supplier Item Directive', {supplier: frm.doc.supplier,item_code:row.item_code}, ['remarks','name'])

d2e0f2847ee67018e265c9051a32248da23e2c40
ashish-greycube <mr.ashish.shah@gmail.com>
2022-03-08 10:09:44 +0530

    ID 77: fix col m, col r, col u, col ab

--- art_collections/art_collections/report/item_trail/item_trail.py
+++ art_collections/art_collections/report/item_trail/item_trail.py
@@ -95,9 +95,12 @@ and SI.docstatus =1
 group by SI_item.item_code  
 order by SUM(SI_item.net_amount) DESC 
 ),
-col_m as (SELECT  PO_item.item_code  , SUM(PO_item.stock_qty) - SUM(PO_item.received_qty) as total_po_qty_to_be_received FROM  `tabPurchase Order Item` as PO_item
+col_total_po_qty_to_be_received as (SELECT  PO_item.item_code  , SUM(PO_item.stock_qty) - SUM(PO_item.received_qty) as total_po_qty_to_be_received FROM  `tabPurchase Order Item` as PO_item
 where PO_item.docstatus = 1
 group by PO_item.item_code
+),
+col_sold_qty_to_deliver as (select sum(so_item.stock_qty-so_item.delivered_qty) as sold_qty_to_deliver,item_code from `tabSales Order` so inner join `tabSales Order Item` so_item on so_item.parent =so.name 
+where so.status in ("To Deliver and Bill","To Deliver") group by so_item.item_code 
 ),
 col_o as (select B.item_code,
 COALESCE(sum(B.actual_qty),0) as total_saleable_stock 
@@ -134,28 +137,32 @@ col_i.qty_sold_in_financial_year,
 col_j.revenue_for_last_12_months,
 col_k.qty_sold_in_last_12_months,
 col_l.avg_qty_sold_per_month,
-col_m.total_po_qty_to_be_received,
+(((IFNULL(col_o.total_saleable_stock,0)-IFNULL(col_sold_qty_to_deliver.sold_qty_to_deliver,0))+IFNULL(col_total_po_qty_to_be_received.total_po_qty_to_be_received,0))/NULLIF(col_l.avg_qty_sold_per_month,0)) as avec_appro,
+col_total_po_qty_to_be_received.total_po_qty_to_be_received,
 fn.availability_date_art,
 col_o.total_saleable_stock,
+IFNULL(col_total_po_qty_to_be_received.total_po_qty_to_be_received,0) as po_to_delivered,
 col_p.qty_sold_to_be_delivered,
 col_o.total_saleable_stock-col_p.qty_sold_to_be_delivered as stock_disposable,
+((col_o.total_saleable_stock-col_p.qty_sold_to_be_delivered)+(IFNULL(col_total_po_qty_to_be_received.total_po_qty_to_be_received,0))) as stock_disposable_plus_po_to_delivered,
 (col_o.total_saleable_stock-col_p.qty_sold_to_be_delivered)/NULLIF(col_l.avg_qty_sold_per_month,0)  as nbr_mois,
 col_s.months_since_first_purchase_receipt,
-((col_l.avg_qty_sold_per_month*8)+col_p.qty_sold_to_be_delivered)-(col_o.total_saleable_stock+col_m.total_po_qty_to_be_received) as commander,
+((col_l.avg_qty_sold_per_month*8)+col_p.qty_sold_to_be_delivered)-(col_o.total_saleable_stock+col_total_po_qty_to_be_received.total_po_qty_to_be_received) as commander,
 IF((col_s.months_since_first_purchase_receipt<=12),
-(col_l.avg_qty_sold_per_month+((col_l.avg_qty_sold_per_month*8)+col_p.qty_sold_to_be_delivered)-(col_o.total_saleable_stock+col_m.total_po_qty_to_be_received)),
-(((col_l.avg_qty_sold_per_month*8)+col_p.qty_sold_to_be_delivered)-(col_o.total_saleable_stock+col_m.total_po_qty_to_be_received)+col_u_last_month.last_year_same_month_stock ))
+(col_l.avg_qty_sold_per_month+((col_l.avg_qty_sold_per_month*8)+col_p.qty_sold_to_be_delivered)-(col_o.total_saleable_stock+col_total_po_qty_to_be_received.total_po_qty_to_be_received)),
+(((col_l.avg_qty_sold_per_month*8)+col_p.qty_sold_to_be_delivered)-(col_o.total_saleable_stock+col_total_po_qty_to_be_received.total_po_qty_to_be_received)+col_u_last_month.last_year_same_month_stock ))
 as col_u
 from fn left outer join col_l on col_l.item_code = fn.name 
 left outer join col_i on col_i.item_code =fn.name 
 left outer join col_k on col_k.item_code =fn.name 
 left outer join col_j on col_j.item_code =fn.name 
-left outer join col_m on col_m.item_code =fn.name 
+left outer join col_total_po_qty_to_be_received on col_total_po_qty_to_be_received.item_code =fn.name 
 left outer join col_o on col_o.item_code =fn.name 
 left outer join col_p on col_p.item_code =fn.name
 left outer join col_s on col_s.item_code =fn.name
 left outer join col_u_last_month on col_u_last_month.item_code =fn.name
 left outer join col_e on col_e.item_code =fn.name and col_e.rn=1
+left outer join col_sold_qty_to_deliver on col_sold_qty_to_deliver.item_code=fn.name
 """,		values = {
 			'from_date': filters.from_date,
 			'to_date': filters.to_date,
@@ -235,7 +242,7 @@ def get_columns(filters):
             "width": 50
         },		
         {
-            "label": _("M:AVEC APPRO"),
+            "label": _("Avec Appro"),
             "fieldtype": "Float",
             "fieldname": "avec_appro",
             "width": 120
@@ -265,7 +272,7 @@ def get_columns(filters):
             "width": 140
         },		
         {
-            "label": _("R:po_to_delivered"),
+            "label": _("Total Qty in PO to deliver"),
             "fieldtype": "Float",
             "fieldname": "po_to_delivered",
             "width": 120
@@ -283,9 +290,9 @@ def get_columns(filters):
             "width": 140
         },	
         {
-            "label": _("U:"),
+            "label": _("Disposable Plus PO to deliver"),
             "fieldtype": "Float",
-            "fieldname": "t_plus_r",
+            "fieldname": "stock_disposable_plus_po_to_delivered",
             "width": 120
         },		
 
@@ -319,23 +326,17 @@ def get_columns(filters):
             "width": 200
         },
         {
-            "label": _("AB:réappro"),
+            "label": _("Réappro"),
             "fieldtype": "Float",
-            "fieldname": "ab",
+            "fieldname": "commander",
             "width": 120
         },	
 	        {
             "label": _("AD:best_customer"),
             "fieldname": "best_customer",
             "width": 200
-        },
-        {
-            "label": _("Total PO qty to be received"),
-            "fieldtype": "Float",
-            "fieldname": "total_po_qty_to_be_received",
-            "width": 140
-        }	
-																						
+        }
+																					
     ]
 
     return columns	

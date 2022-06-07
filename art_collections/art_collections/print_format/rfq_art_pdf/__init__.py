881d3f01d7d4ff7929c81e93743d8b3c61454589
vr-greycube <vijay@greycube.in>
2022-06-07 16:02:52 +0530

    minor: rfq pdf _ get supplier_part_no from item>suppliers

--- art_collections/art_collections/print_format/rfq_art_pdf/__init__.py
+++ art_collections/art_collections/print_format/rfq_art_pdf/__init__.py
@@ -28,13 +28,15 @@ def get_print_context(name, supplier):
             i.item_name, 
             i.item_code, 
             tib.barcode,
-            trfqi.supplier_part_no , 
+            tis.supplier_part_no , 
             i.customs_tariff_number ,
             trfqi.qty, 
             trfqi.stock_uom 
         from `tabRequest for Quotation` trfq
         inner join `tabRequest for Quotation Item` trfqi on trfqi.parent = trfq.name
         inner join tabItem i on i.name = trfqi.item_code
+        left outer join `tabItem Supplier` tis on tis.parent = i.item_code 
+            and tis.supplier = %(supplier)s
         left outer join `tabItem Barcode` tib on tib.parent = i.name 
             and tib.idx  = (
                 select min(idx) from `tabItem Barcode` tib2
@@ -42,7 +44,7 @@ def get_print_context(name, supplier):
             )  
         where trfq.name = %(name)s
     """,
-            dict(name=name),
+            dict(name=name, supplier=supplier),
             as_dict=True,
         )
     )

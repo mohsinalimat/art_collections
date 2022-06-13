7a7e1169369d011d9a121ff8c320ec76937a3e0b
vr-greycube <vijay@greycube.in>
2022-06-13 13:57:02 +0530

    minor: container followup add columns for supplier, set_apart_art

--- art_collections/art_collections/report/container_admission_follow_up/container_admission_follow_up.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022, GreyCube Technologies and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Container admission follow up"] = {
-	"filters": [
-
-	]
-};
--- art_collections/art_collections/report/container_followup/container_followup.py
+++ art_collections/art_collections/report/container_followup/container_followup.py
@@ -18,7 +18,7 @@ select
                 tasc.arrival_forecast_date , tasc.arrival_forecast_hour , tas.telex_release_sent_date ,
                 tasc.total_outer_qty , 
                 case when nullif(spl.pr_name,'') is not null then 'Yes' else 'No' end is_container_recieved ,
-                spl.spl_name , spl.supplier , spl.po_name , if(spl.set_apart_art=1,'Yes','No') set_apart_art , 
+                spl.spl_name , spl.supplier , spl.po_name , if(spl.set_apart_art=1,1,0) set_apart_art , 
                 spl.container_count , spl.pr_name , spl.pr_address_title ,
                 case when tasc.type_of_reception = 'FCL' then tasc.container_size
                 	else tasc.qty_of_pallet end transport_size
@@ -27,7 +27,7 @@ select
                 left outer join `tabArt Shipment Container` tasc on tasc.parent = tas.name
                 left outer join (
                     select det.container , count(det.container) container_count, 
-                    GROUP_CONCAT(DISTINCT p.supplier ORDER BY p.supplier) supplier , 
+                    GROUP_CONCAT(DISTINCT concat(p.supplier,':',tsup.supplier_name) ORDER BY p.supplier) supplier , 
                     GROUP_CONCAT(DISTINCT det.parent ORDER BY det.creation) spl_name , 
                     GROUP_CONCAT(DISTINCT det.purchase_order ORDER BY tpo2.transaction_date ) po_name ,
                     max(set_apart_art) set_apart_art ,
@@ -35,6 +35,7 @@ select
                     GROUP_CONCAT(DISTINCT ta.address_title) pr_address_title 
                     from `tabSupplier Packing List Detail Art` det
                     inner join `tabSupplier Packing List Art` p on p.name = det.parent 
+                    left outer join tabSupplier tsup on tsup.name = p.supplier
                     left outer join `tabPurchase Order` tpo2 on tpo2.name = det.purchase_order
                     left outer join `tabPurchase Receipt Item` tpri on tpri.purchase_order = tpo2.name
                     left outer join `tabPurchase Receipt` tpr on tpr.name = tpri.parent 
@@ -60,12 +61,14 @@ def get_columns(filters):
         Container #,container_name,,,145
         Received is for Container,is_container_recieved,,130
         SPL #,spl_name,,,200
+        Supplier,supplier,,,200
         PO,po_name,,,200
         Outer Qty,total_outer_qty,Int,,130
         Total Qty,container_count,Int,,130
         Transport Type,type_of_reception,,,130
         Transport Size,transport_size,,,130
         Shipping Date,shipping_date,Date,,130
+        Set Apart,set_apart_art,Int,,130
         Arrival Forecast Date,arrival_forecast_date,Date,,130
         Arrival Forecast Hour,arrival_forecast_hour,,,130
         Purchase Receipt,pr_name,,,200

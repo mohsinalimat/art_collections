8deae7a6c3a71b22b6614f2af07b7ef5e7a9e032
ashish-greycube <mr.ashish.shah@gmail.com>
2022-11-02 12:05:38 +0530

    patch : remove supplier : default_terms_and_conditions_art

--- art_collections/patches/v0_3/drop_supplier_column.py
+++ art_collections/patches/v0_3/drop_supplier_column.py
@@ -1,7 +1,11 @@
-# import frappe
+import frappe
 
-# def execute():
-#   if frappe.db.exists("DocType", "Supplier"):
+def execute():
+  if frappe.db.exists("DocType", "Supplier"):
+
+    frappe.reload_doc("buying", "doctype", "supplier")
+    frappe.delete_doc_if_exists("Supplier", "default_terms_and_conditions_art")
+    frappe.delete_doc_if_exists("Custom Field", "Supplier-default_terms_and_conditions_art")     
     
 #     frappe.reload_doc("buying", "doctype", "supplier")
 #     frappe.delete_doc_if_exists("Supplier", "subledger_account")
